7631044b7414f991592163c4caca867bf7874a44
Tom Kim <babooteng2@gmail.com>
2021-08-15 17:03:58 +0900

    #0.6 Extending the Schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,13 @@
+const babooteng2 = {
+    name: "babooteng2",
+    age: 18,
+    gender: "female"
+}
+
 const resolvers = {
     Query: {
-        name: () => "babooteng2"
+        name: () => "babooteng2",
+        person: () => babooteng2
     }
 };
 
